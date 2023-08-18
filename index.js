92bd6dc9fc557d18cd8f5666435ade781c2f95c0
Safoun <safoun.official@gmail.com>
2023-08-18 18:03:12 +0600

    experimenting

--- index.js
+++ index.js
@@ -10,6 +10,19 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+// /////////////////////////////////////////////////////////////////////////////////////////
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST,PATCH');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+// /////////////////////////////////////////////////////////////////////////////////////////
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@techtitans.gvuoct6.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,10 +37,21 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server    (optional starting in v4.7)
-    await client.connect();
+    client.connect();
     // Send a ping to confirm a successful connection
 
     const allGames = client.db("titanArena").collection("games");
+    const testGames = client.db("titanArena").collection("test");
+
+
+
+    ///////////////////////////////////////////////////////////////////////
+    app.get("/test", async (req, res) => {
+      const colleges = testGames.find();
+      const result = await colleges.toArray();
+      res.send(result);
+    });
+    // ///////////////////////////////////////////////////////////////////////
 
     // Nabil brach
     app.get("/games", async (req, res) => {
@@ -76,7 +100,9 @@ async function run() {
     // await client.close();
   }
 }
-run().catch(console.dir);
+run().catch((error) => {
+  console.error("Error during server startup:", error);
+});
 
 app.listen(port, (req, res) => {
   console.log("Titans Arena is sitting on port", port);
