46591557de4b16f1f465ffff2ca5968267a1bbf8
jakariachynabil10 <jakariachynabil@gmail.com>
2023-09-08 17:01:57 +0600

    code organaize

--- NABIL/UserInfo.js
+++ NABIL/UserInfo.js
@@ -2,26 +2,16 @@ const { ObjectId } = require("mongodb");
 
 const UserInfo = async (req, res, usersCollection) => {
   const email = req.params.email;
-//   console.log(email)
-
-  // Convert the userId string to ObjectId
-  const query = {email : email}
-//   console.log(query)
-  
-
-  // Find the user document by ObjectId
+  const query = { email: email };
   const user = await usersCollection.findOne(query);
 
   if (!user) {
     return res.status(404).json({ message: "User not found" });
   }
+  const userInfo = user;
+  console.log(userInfo);
 
-  // Assuming your user document has a 'role' field, you can access it
-  const userAllInfo = user;
-  console.log(userAllInfo)
-
-  // Respond with the user's role
-  res.send({userAllInfo});
+  res.send({ userInfo });
 };
 
-module.exports = {  UserInfo };
+module.exports = { UserInfo };
